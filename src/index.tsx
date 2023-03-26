3f1ef8aa2e2f5545c2e25ccfdbdd7b9f6b1d34c2
fliseno1k <fliseno1k@gmail.com>
2023-03-27 09:49:19 +1000

    styles: apply base styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import s from './style.module.scss';
-
-function App() {
-    return (
-        <div className={s._}>
-            <h1 className={'s'}>Hello React!</h1>
-        </div>
-    );
-}
+import { App } from './app';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
